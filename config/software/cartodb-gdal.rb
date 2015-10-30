013d6febed350897b8e16d8f0fc0e8148724a521
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-30 14:45:35 -0400

    Update cartodb-gdal.rb

--- config/software/cartodb-gdal.rb
+++ config/software/cartodb-gdal.rb
@@ -64,9 +64,4 @@ build do
 
   make "-j #{workers}", cwd: "#{project_dir}/gdal", env: with_standard_compiler_flags(with_embedded_path)
   make 'install', cwd: "#{project_dir}/gdal"
-  
-  # build test binary and run 
-  make "-j #{workers} all", cwd: "#{project_dir}/gdal/apps", env: with_standard_compiler_flags(with_embedded_path)
-  copy "#{project_dir}/gdal/apps/test_ogrsf", "#{install_dir}/embedded/bin/"
-  command './run_all.py alg', cwd: "#{project_dir}/autotest", env: with_embedded_path
 end
